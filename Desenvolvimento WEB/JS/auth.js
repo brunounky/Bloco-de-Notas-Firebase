6159b9ab0c88c0572360497e151afcef0a2e4dca
brunounky <bruno_unky@hotmail.com>
2021-06-18 19:09:23 -0300

    Update README.md

---
+++ Desenvolvimento WEB/JS/auth.js
@@ -0,0 +1,15 @@
+
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB16xY4tnjj4MHgrNEznBJ4cCPMpUU_aws",
+    authDomain: "bloco-de-notas-firebase-9788f.firebaseapp.com",
+    projectId: "bloco-de-notas-firebase-9788f",
+    storageBucket: "bloco-de-notas-firebase-9788f.appspot.com",
+    messagingSenderId: "381868789094",
+    appId: "1:381868789094:web:0e56892839a12a05ee0ca7",
+    measurementId: "G-G5TC14B6PF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
